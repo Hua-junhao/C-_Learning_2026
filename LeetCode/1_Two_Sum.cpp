8c8222a2db8a399ab6d9c566be601164f8deec31
Hua-junhao <1249639696@qq.com>
2026-01-06 13:38:10 +0800

    Optimize Two Sum using Hash Map for O(n) performance

--- LeetCode/1_Two_Sum.cpp
+++ LeetCode/1_Two_Sum.cpp
@@ -1,12 +1,33 @@
+//1.双重循环暴力枚举(Brute Force)
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//     for (int i=0;i<nums.size();i++)
+//     {
+//         for (int j=i+1;j<nums.size();j++)
+//         {
+//             if(nums[i]+nums[j]==target) return{i,j};
+//         }
+//     }return{};    
+//     }
+// };
+//2.哈希表
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    for (int i=0;i<nums.size();i++)
-    {
-        for (int j=i+1;j<nums.size();j++)
+        unordered_map<int, int> hashTable;
+        
+        for (int i = 0; i < nums.size(); i++) 
         {
-            if(nums[i]+nums[j]==target) return{i,j};
+            
+            int complement = target - nums[i];
+            if (hashTable.find(complement) != hashTable.end()) 
+            {
+                return {hashTable[complement], i};
+            }
+            hashTable[nums[i]] = i;
         }
-    }return{};    
+        
+        return {};
     }
 };
