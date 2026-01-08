01b801785d385168311edb2ae5db62d6c2e0b3b3
Hua-junhao <1249639696@qq.com>
2026-01-08 23:49:21 +0800

    optimize LC #3 using sliding window with left-pointer protection

--- LeetCode/3_Longest_Substring_Without_Repeating_Characters.cpp
+++ LeetCode/3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,36 +1,57 @@
- class Solution {
-public:
-    bool isdifferent(const string &s,int start,int end)
-    {
-        for (int i=start;i<end;i++)
-        {
-            for (int j=i+1;j<=end;j++)
-            {
-                if (s[j]==s[i]) 
-                {
-                    return 0;
-                }
-            }
-        }
-        return 1;
+//  class Solution {
+// public:
+//     bool isdifferent(const string &s,int start,int end)
+//     {
+//         for (int i=start;i<end;i++)
+//         {
+//             for (int j=i+1;j<=end;j++)
+//             {
+//                 if (s[j]==s[i]) 
+//                 {
+//                     return 0;
+//                 }
+//             }
+//         }
+//         return 1;
 
-    }
-    int lengthOfLongestSubstring(string s) 
-    { 
-        int maxlength=1;
+//     }
+//     int lengthOfLongestSubstring(string s) 
+//     { 
+//         int maxlength=1;
         
-        if (s.size()==0) return 0;
-        for (int i=0;i<s.size();i++)
+//         if (s.size()==0) return 0;
+//         for (int i=0;i<s.size();i++)
+//         {
+//             for (int j=i+1;j<s.size();j++)
+//             {
+//                 if (isdifferent(s,i,j))
+//                 {
+//                     maxlength=max(maxlength,j-i+1);
+//                 }
+//             }
+//         }
+//         return maxlength;
+//     }
+    
+// };
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int>character;
+        int maxlength=1;
+        if (s.empty()) return 0;
+        if (s.size()==1) return 1;
+        int left=0;
+        character[s[left]]=left;
+        for (int right=1;right<s.size();right++)
         {
-            for (int j=i+1;j<s.size();j++)
+            if (character.count(s[right]))//判断是否出现过
             {
-                if (isdifferent(s,i,j))
-                {
-                    maxlength=max(maxlength,j-i+1);
-                }
+                left=max(character[s[right]]+1,left);
             }
+            character[s[right]]=right;
+            maxlength=max(maxlength,right-left+1);
         }
         return maxlength;
     }
-    
 };
