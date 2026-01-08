2959bd30e1e2c97bcea854e81cc523d6b27aff97
Hua-junhao <1249639696@qq.com>
2026-01-08 23:55:16 +0800

    style: clean up commented out code

--- LeetCode/3_Longest_Substring_Without_Repeating_Characters.cpp
+++ LeetCode/3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,39 +1,3 @@
-//  class Solution {
-// public:
-//     bool isdifferent(const string &s,int start,int end)
-//     {
-//         for (int i=start;i<end;i++)
-//         {
-//             for (int j=i+1;j<=end;j++)
-//             {
-//                 if (s[j]==s[i]) 
-//                 {
-//                     return 0;
-//                 }
-//             }
-//         }
-//         return 1;
-
-//     }
-//     int lengthOfLongestSubstring(string s) 
-//     { 
-//         int maxlength=1;
-        
-//         if (s.size()==0) return 0;
-//         for (int i=0;i<s.size();i++)
-//         {
-//             for (int j=i+1;j<s.size();j++)
-//             {
-//                 if (isdifferent(s,i,j))
-//                 {
-//                     maxlength=max(maxlength,j-i+1);
-//                 }
-//             }
-//         }
-//         return maxlength;
-//     }
-    
-// };
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
