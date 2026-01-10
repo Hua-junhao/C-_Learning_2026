8083769cfebca7b508a81fffd68d243f9d1848b4
Hua-junhao <1249639696@qq.com>
2026-01-10 20:39:17 +0800

    perf: optimize LeetCode #5 to O(n^2) using Center Expansion

--- LeetCode/5.longest-palindromic-substring.cpp
+++ LeetCode/5.longest-palindromic-substring.cpp
@@ -6,37 +6,43 @@
 
 // @lc code=start
 class Solution {
-public:
-     bool isPalindrome(const string  &s,int left,int right)
-    {
-        while (left<right)
+private :
+    int expand (const string &s,int left ,int right)
+    {   while (true)
         {
-            if (s[left]!=s[right]) return 0;
-            left++;
-            right--;
+            if (left <0||right>=s.size()||s[left]!=s[right])
+            {
+             return (right-left-1);
+            }
+            else
+            {
+                right ++;
+                left --;
+            }
         }
-        return 1;
+        
     }
+public:
     string longestPalindrome(string s) 
     {
-        if (s.size()==1) return s;
-        string maxs=s.substr(0,1);
-        for (int left=0;left<s.size()-1;left++)
-        { 
-            string news="";
-           for (int right=left+1;right<s.size();right++)
-           {
-            if (isPalindrome(s,left,right))
+        if (s.size()<2) return s;
+        int start =0;
+        int maxlen=1;
+        for (int i=0;i<s.size();i++)
+        {
+            int len1=expand(s,i,i);
+            int len2=expand(s,i,i+1);
+            int len=max(len1,len2);
+            if (len>maxlen)
             {
-                news=s.substr(left,right-left+1);
+                maxlen=len;
+                if (len%2==0) start=i-len/2+1;
+                else start =i-len/2;
             }
-            if (news.size()>maxs.size())
-            {
-                maxs=news;
-            }
-           }
         }
-        return maxs;
+        string ls=s.substr(start,maxlen);
+        return ls;
+
         
     }
 };
