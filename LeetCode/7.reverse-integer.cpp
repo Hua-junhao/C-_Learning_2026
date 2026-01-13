cb453596d267c0089feea144b6b04ce72176cec0
Hua-junhao <1249639696@qq.com>
2026-01-13 21:17:20 +0800

    feat: solve LeetCode #7 using 32-bit logic without long long

---
+++ LeetCode/7.reverse-integer.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=7 lang=cpp
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int reverse(int x) {
+    int  num=0;
+    while (x!=0) 
+    {
+       int a=x%10;
+       x=x/10;
+       if (num>INT_MAX/10||(num==INT_MAX/10&&a>7)) return 0;
+       if (num<INT_MIN/10||(num==INT_MIN/10&&a<-8)) return 0;
+       num=num*10+a;
+    }
+    return num; 
+    }
+};
+// @lc code=end
+
