80584c6f50b4cbd7bd13b26509a6d898b60ccfd4
Hua-junhao <1249639696@qq.com>
2026-01-15 20:14:33 +0800

    perf: optimize LeetCode 11 to O(N) using Greedy Two Pointers

--- LeetCode/11.container-with-most-water.cpp
+++ LeetCode/11.container-with-most-water.cpp
@@ -8,18 +8,17 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+    int left =0;
     int v=0;
-    for (int left=0;left<height.size()-1;left++)
-    {   
-        int right=height.size()-1;
-        while (right>left)
-        {
-            int v1=min(height[left],height[right])*(right-left);
-            if(v1>v) v=v1;
-            right--;
-        }
+    int right =height.size()-1;
+    while (left<right)
+    {
+        int v1=min(height[left],height[right])*(right-left);
+        if(v1>v) v=v1;
+        if(height[left]>height[right]) right--;
+        else left++;
     }
-      return v;  
+    return v;
     }
 };
 // @lc code=end
