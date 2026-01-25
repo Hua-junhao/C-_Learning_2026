12bb32156dee14e8c72b603b0eb5c063b324cfdf
Hua-junhao <1249639696@qq.com>
2026-01-25 17:04:08 +0800

    feat: AC LeetCode 33 using Modified Binary Searchfeat: AC LeetCode 33 using Modified Binary Search
    - Implement O(log N) search for rotated sorted array
    - Use mid calculation 'l + (r - l) / 2' to prevent integer overflow
    - Handle boundary case 'nums[mid] >= nums[l]' to correctly identify sorted halves

--- LeetCode/33.search-in-rotated-sorted-array.cpp
+++ LeetCode/33.search-in-rotated-sorted-array.cpp
@@ -11,19 +11,35 @@ public:
         int l=0,r=nums.size()-1;
         while(l<=r)
         {
-            if(nums[l]==target)
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)
             {
-                return l;
+                return mid;
             }
-            else if (nums[r]==target)
+            if(nums[mid]>=nums[l])//nums[mid]==nums[l]时，左半边有序(eg.[3,1])
             {
-                return r;
+                if(nums[l]<=target&&nums[mid]>target)
+                {
+                    r=mid-1;
+                }
+                else
+                {
+                    l=mid+1;
+                }
             }
-            r--;
-            l++;
+            else
+            {
+                if(nums[r]>=target&&nums[mid]<target)
+                {
+                    l=mid+1;
+                }
+                else
+                {
+                    r=mid-1;
+                }
+            }
         }
         return -1;
-        
     }
 };
 // @lc code=end
