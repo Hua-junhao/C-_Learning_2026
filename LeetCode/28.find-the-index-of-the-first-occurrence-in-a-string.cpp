46b17dae0c86031217f70a9ad241da70620410e7
Hua-junhao <1249639696@qq.com>
2026-01-28 16:50:21 +0800

    feat: solve LeetCode 28 using KMP algorithm for O(N+M) complexity
    - Implement KMP (Knuth-Morris-Pratt) pattern matching algorithm
    - Precompute LPS (Longest Prefix Suffix) array to allow backtracking
    - Optimize time complexity from O(N*M) to O(N+M) by avoiding redundant checks

--- LeetCode/28.find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ LeetCode/28.find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -7,19 +7,38 @@
 // @lc code=start
 class Solution {
 public:
+    void getNext(int* next, const string & s) {
+        int j = 0;
+        next[0] = 0; 
+        for(int i = 1; i < s.size(); i++) {
+            while (j > 0 && s[i] != s[j]) {
+                j = next[j - 1]; 
+            }
+            if (s[i] == s[j]) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
     int strStr(string haystack, string needle) {
-        int n1=haystack.size();
-        int n2=needle.size();
-        for(int i=0;i<n1-n2+1;i++)
-        {
-            string a =haystack.substr(i,n2);
-            if(a==needle)
-            {
-                return i;
+        if (needle.size() == 0) return 0;
+        int next[needle.size()];
+        getNext(next, needle); 
+        int j = 0; 
+        for (int i = 0; i < haystack.size(); i++) {
+            while (j > 0 && haystack[i] != needle[j]) {
+                j = next[j - 1]; 
+            }
+            if (haystack[i] == needle[j]) {
+                j++;
+            }
+            if (j == needle.size()) {
+                return i - needle.size() + 1; 
             }
         }
         return -1;
     }
+    
 };
 // @lc code=end
 
