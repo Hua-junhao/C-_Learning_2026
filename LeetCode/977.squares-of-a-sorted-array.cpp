eb98897f0f44240ea150d7187e510ff750fdbd3c
Hua-junhao <1249639696@qq.com>
2026-02-01 18:33:02 +0800

    feat: solve LeetCode 977 using brute force squaring and sort
    - Calculate squares of all elements in place
    - Sort the array using Bubble Sort implementation
    - Note: Time complexity is O(N^2), pending optimization to O(N)

---
+++ LeetCode/977.squares-of-a-sorted-array.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=977 lang=cpp
+ *
+ * [977] Squares of a Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=pow(nums[i],2);
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    swap(nums[j],nums[j+1]);
+                }
+            }
+        }
+        return nums;
+        
+    }
+};
+// @lc code=end
+
