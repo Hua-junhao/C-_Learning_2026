48fa7f79fc163d9a93c0f753d9c2e98f22e7a7fb
Hua-junhao <1249639696@qq.com>
2026-02-06 11:25:46 +0800

    refactor: optimize LeetCode 541 loop logic using std::min
    - Replace explicit if-else boundary checks with std::min() inside std::reverse
    - Switch to a for-loop with '2*k' increment for better readability
    - Simplify logic while maintaining O(N) time complexity

--- LeetCode/541.reverse-string-ii.cpp
+++ LeetCode/541.reverse-string-ii.cpp
@@ -8,18 +8,10 @@
 class Solution {
 public:
     string reverseStr(string s, int k) {
-        int i=0;
-        while(i<s.size())
+        int i=0,n=s.size();
+        for(int i=0;i<n;i+=2*k)
         {
-            if(i+k<=s.size())
-            {
-                reverse(s.begin()+i,s.begin()+i+k);
-            }
-            else
-            {
-                reverse(s.begin()+i,s.end());
-            }
-            i+=2*k;
+            reverse(s.begin()+i,s.begin()+min(i+k,n));
         }
         return s;
     }
