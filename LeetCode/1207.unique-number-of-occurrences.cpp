80e95eb04792bcc962c24c6ae95756fd80757445
Hua-junhao <1249639696@qq.com>
2026-02-11 01:34:23 +0800

    refactor: optimize using Map and Set (O(N))
    Removed unnecessary Sorting (O(N log N)) and Vector Search (O(K^2)).
    Implemented a standard frequency check using unordered_map for counting and unordered_set for uniqueness verification.
    Improved logic clarity and performance.

--- LeetCode/1207.unique-number-of-occurrences.cpp
+++ LeetCode/1207.unique-number-of-occurrences.cpp
@@ -9,44 +9,16 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int>map;
-        sort(arr.begin(),arr.end());
-        int n=arr.size();
-        int l=0;
-        for(int i=0;i<n;i++)
+        unordered_set<int>set;
+        for(int i:arr)
         {
-            if(i==0)
-            {
-                map[arr[i]]++;
-                l++;
-            }
-            else if(arr[i]==arr[i-1])
-            {
-                map[arr[i]]++;
-            }
-            else
-            {
-                arr[l]=arr[i];
-                l++;
-                map[arr[i]]++;
-            }
+            map[i]++;
         }
-        vector<int>v;
-        for(int i=0;i<l;i++)
+        for(auto& pair:map)
         {
-            if(i==0)
-            {
-                v.push_back(map[arr[i]]);
-            }
-            else if(find(v.begin(),v.end(),map[arr[i]])!=v.end())
-            {
-                return false;
-            }
-            else 
-            {
-                v.push_back(map[arr[i]]);
-            }
+            set.insert(pair.second);
         }
-        return true;
+        return set.size()==map.size();
         
     }
 };
