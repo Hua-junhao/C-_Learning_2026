daec37c2e5ec1c49c9820ed7fcfa50d7e871a10a
Hua-junhao <1249639696@qq.com>
2026-02-10 19:32:56 +0800

    refactor: impl Two Pointers (Meet-at-Peak) strategy
    Replaced linear scan with a dual-pointer approach.
    - Left pointer climbs up.
    - Right pointer climbs up.
    - Validates that both pointers meet at the same peak index (not start or end).
    Time Complexity: O(N)
    Space Complexity: O(1)

--- LeetCode/941.valid-mountain-array.cpp
+++ LeetCode/941.valid-mountain-array.cpp
@@ -8,41 +8,28 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        bool flag=1,flag2=1;
-        for(int i=1;i<arr.size();i++)
-        {
-            if(arr[i]==arr[i-1])
-            {
-                return false;
-            }
-            if(flag&&arr[i]>arr[i-1])
-            {
-                flag2=0;
-                continue;
-            }
-            if(flag&&arr[i]<arr[i-1])
-            {
-                flag=0;
-                continue;
-            }
-            if(flag==0&&arr[i]>arr[i-1])
-            {
-                return false; 
-            }
-            if(flag==0&&arr[i]<arr[i-1])
-            {
-                continue;
-            }
-        }
-        if(flag||flag2)
+        int n=arr.size();
+        int l=0,r=n-1;
+        if(n<3)
         {
             return false;
-        } 
+        }
+        while(l<r&&arr[l]<arr[l+1])
+        {
+            l++;
+        }
+        while(r>l&&arr[r]<arr[r-1])
+        {
+            r--;
+        }
+        if(l==0||r==n-1||l!=r)
+        {
+            return false;
+        }
         else
         {
             return true;
         }
-        
     }
 };
 // @lc code=end
