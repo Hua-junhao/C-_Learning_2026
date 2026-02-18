163a780e2bed6cfbe595ac5a565a3322f60a17fa
Hua-junhao <1249639696@qq.com>
2026-02-18 16:28:32 +0800

    refactor: apply Dummy Head technique to LC203
    Replaced separate head/body logic with a Dummy Head (Virtual Node) approach.
    - Unified the removal logic into a single loop.
    - Maintained memory safety by deleting removed nodes and the dummy node.
    - Improved code readability and maintainability.

--- LeetCode/203.remove-linked-list-elements.cpp
+++ LeetCode/203.remove-linked-list-elements.cpp
@@ -18,27 +18,26 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        while(head!=NULL&&head->val==val)
-        {
-            ListNode* tmp=head;
-            head=head->next;
-            delete tmp;
-        }
-        ListNode* cur =head;
-        while (cur!=NULL&&cur->next!=NULL)
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* cur=dummy;
+        while (cur->next!=NULL)
         {
             if(cur->next->val==val)
             {
                 ListNode* tmp=cur->next;
-                cur->next=cur->next->next;
+                cur->next=tmp->next;
                 delete tmp;
             }
-            else
+            else 
             {
                 cur=cur->next;
-            }   
-        } 
-        return  head;
+            }
+        }
+        head=dummy->next;
+        delete dummy;
+        return head;
+        
     }
 };
 // @lc code=end
