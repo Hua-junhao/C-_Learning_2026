1bc1d592ca0c95dec3ee68721ff08eac2e731df9
Hua-junhao <1249639696@qq.com>
2026-02-22 17:01:00 +0800

    refactor: streamline LC15 3Sum two-pointer logic
    Optimized the existing O(N^2) Two Pointers solution.
    - Replaced multiple independent 'if' statements with strictly structured 'if-else if-else' branches to prevent logic hazards.
    - Removed unnecessary inner while-loop deduplications for strictly greater/lesser sums, improving code readability.
    - Simplified mathematical sum evaluation by removing redundant long long casting and multiplier inversion.
    Time Complexity: O(N^2)
    Space Complexity: O(1)

--- LeetCode/15.3-sum.cpp
+++ LeetCode/15.3-sum.cpp
@@ -13,42 +13,25 @@ public:
         vector<vector<int>>result;
         for(int i=0;i<n-2;i++)
         {    
-            if(nums[i]>0){
-                break;
-            }
+            if(nums[i]>0) break;
+            if(i>0&&nums[i]==nums[i-1]) continue;
             int l=i+1;
             int r=n-1;
-            if(i>0&&nums[i]==nums[i-1]){
-                continue;
-            }
             while(l<r)
             {
-                long long sum=nums[l]+nums[r];
-                if(sum==(-1)*nums[i]){
-                    result.push_back({nums[i],nums[l],nums[r]});
+                int sum=nums[l]+nums[r]+nums[i];
+                if(sum>0){
+                    r--;
+                }
+                else if(sum<0){
+                    l++;
+                }
+                else{
+                    result.push_back({nums[i], nums[l], nums[r]});
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
                     l++;
                     r--;
-                    while(l<r&&nums[l]==nums[l-1]){
-                        l++;
-                    }
-                    while(l<r&&nums[r]==nums[r+1]){
-                        r--;
-                    }
-                    if(l>=r){
-                        break;
-                    }
-                }
-                if(sum>(-1)*nums[i]){
-                    r--;
-                    while(l<r&&nums[r]==nums[r+1]){
-                        r--;
-                    }
-                }
-                if(sum<(-1)*nums[i]){
-                    l++;
-                    while(l<r&&nums[l]==nums[l-1]){
-                        l++;
-                    }
                 }
             }
         }
