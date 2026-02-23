54eccce013513273c1d832a6cd445b517f24b76a
Hua-junhao <1249639696@qq.com>
2026-02-23 16:32:48 +0800

    feat: solve LC347 Top K Frequent Elements using Min-Heap with Custom Comparator
    Implemented a priority queue with a custom functor to sort elements by frequency.
    - Used unordered_map to count element occurrences.
    - Defined 'mycomparison' struct to override default pair comparison (sorting by .second instead of .first).
    - Maintained a Min-Heap of size K to efficiently filter the top frequent elements.
    Time Complexity: O(N log K)
    Space Complexity: O(N)

---
+++ LeetCode/347.top-k-frequent-elements.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=347 lang=cpp
+ *
+ * [347] Top K Frequent Elements
+ */
+
+// @lc code=start
+class Solution {
+public:
+    class mycomparison {
+    public:
+        bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs) {
+            return lhs.second > rhs.second; 
+        }
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>map;
+        for(int i:nums)
+        {
+            map[i]++;
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,mycomparison>pri_que;
+        for(auto it=map.begin();it!=map.end();it++)
+        {
+            pri_que.push(*it);
+            if(pri_que.size()>k)
+            {
+                pri_que.pop();
+            }
+        }
+        vector<int>result(k);
+        for(int i=k-1;i>=0;i--)
+        {
+            result[i]=pri_que.top().first;
+            pri_que.pop();
+        }
+        return result;
+    }
+};
+// @lc code=end
+
