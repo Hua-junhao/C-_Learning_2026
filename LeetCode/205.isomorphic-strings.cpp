f7db8aa002384142f01032c5f86a1e2985dc8ba1
Hua-junhao <1249639696@qq.com>
2026-02-26 21:41:14 +0800

    perf: optimize LC205 using Array Index Mapping
    Replaced std::unordered_map with fixed-size integer arrays (int[256]).
    - Utilized the "last seen index" strategy to verify structural isomorphism.
    - Eliminated hashing overhead and simplified logic to a single pass.
    - Achieved reduced constant space and faster runtime.

--- LeetCode/205.isomorphic-strings.cpp
+++ LeetCode/205.isomorphic-strings.cpp
@@ -8,25 +8,15 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>map1;
-        unordered_map<char,char>map2;
+        int arr1[256]={0};
+        int arr2[256]={0};
         int n=s.size();
         if(n!=t.size()) return false;
         for(int i=0;i<n;i++)
         {
-            if(map1.find(s[i])==map1.end()&&map2.find(t[i])==map2.end())
-            {
-                map1[s[i]]=t[i];
-                map2[t[i]]=s[i];
-            }
-            else if(map1[s[i]]==t[i]&&map2[t[i]]==s[i])
-            {
-                continue;
-            }
-            else
-            {
-                return false;
-            }
+            if(arr1[s[i]]!=arr2[t[i]]) return false;
+            arr1[s[i]]=i+1;
+            arr2[t[i]]=i+1;
         }
         return true;
 
