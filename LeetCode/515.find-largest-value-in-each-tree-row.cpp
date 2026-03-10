a97e600ad431797abb92b37042918bb799fb4cd9
Hua-junhao <1249639696@qq.com>
2026-03-10 19:34:59 +0800

    feat(tree): implement BFS to find largest value in each tree row
    - Adapt standard level-order traversal template.
    - Calculate row maximums directly during traversal without intermediate vectors, saving memory allocation overhead.

---
+++ LeetCode/515.find-largest-value-in-each-tree-row.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=515 lang=cpp
+ *
+ * [515] Find Largest Value in Each Tree Row
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int>result;
+        queue<TreeNode*>que;
+        if(root==nullptr) return result;
+        que.push(root);
+        while(!que.empty())
+        {
+            int size=que.size();
+            int max=INT_MIN;
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* cur=que.front();
+                que.pop();
+                if(max<cur->val) max=cur->val;
+                if(cur->left) que.push(cur->left);
+                if(cur->right) que.push(cur->right);
+            }
+            result.push_back(max);
+        }
+        return result;
+        
+    }
+};
+// @lc code=end
+
