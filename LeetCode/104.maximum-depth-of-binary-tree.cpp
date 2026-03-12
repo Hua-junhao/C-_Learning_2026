4f510e971c954de1e6682019c9f53c10b6d8c98e
Hua-junhao <1249639696@qq.com>
2026-03-12 16:22:27 +0800

    refactor(tree): [LC104] optimize space complexity to O(logN) using DFS
    - Replace BFS queue with recursive post-order traversal.
    - Reduce average space complexity from O(N) width to O(logN) height.

--- LeetCode/104.maximum-depth-of-binary-tree.cpp
+++ LeetCode/104.maximum-depth-of-binary-tree.cpp
@@ -19,23 +19,8 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        queue<TreeNode*>que;
-        int maxdepth=0;
-        if(root==nullptr) return maxdepth;
-        que.push(root);
-        while(!que.empty())
-        {
-            int size=que.size();
-            maxdepth++;
-            for(int i=0;i<size;i++)
-            {
-                TreeNode* cur=que.front();
-                que.pop();
-                if(cur->left) que.push(cur->left);
-                if(cur->right) que.push(cur->right);
-            }
-        }
-        return maxdepth;
+        if(root=nullptr) return 0;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
         
     }
 };
