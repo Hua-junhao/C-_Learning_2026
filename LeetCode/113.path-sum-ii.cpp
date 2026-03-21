3dd43243aff9aab88fb68a728ee43151eeb4f4c1
Hua-junhao <1249639696@qq.com>
2026-03-21 15:34:10 +0800

    feat(tree): [LC113] implement path sum ii using backtrack with state recovery
    - Utilize a path vector and targetSum reduction to locate valid paths.
    - Employ single-exit backtracking (`pop_back`) at the end of the recursion to ensure environment restoration.
    - Consolidate leaf-node validation and recursive branch exploration for clarity.

---
+++ LeetCode/113.path-sum-ii.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=113 lang=cpp
+ *
+ * [113] Path Sum II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void findpath(TreeNode*cur,int targetSum,vector<int>&v,vector<vector<int>>&result)
+    {
+        v.push_back(cur->val);
+        targetSum-=cur->val;
+        if(!cur->left && !cur->right)
+        {
+            if(targetSum==0) result.push_back(v);
+        }
+        else
+        {
+            if(cur->left) findpath(cur->left,targetSum,v,result);
+            if(cur->right) findpath(cur->right,targetSum,v,result);
+        }
+        v.pop_back();
+        
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>>result;
+        vector<int>v;
+        findpath(root,targetSum,v,result);
+        return result;
+        
+    }
+};
+// @lc code=end
+
