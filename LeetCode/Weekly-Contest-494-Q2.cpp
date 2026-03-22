78fdc380619002b01591099779708181498cda0d
Hua-junhao <1249639696@qq.com>
2026-03-22 11:33:23 +0800

    perf:[Weekly_494_Q2] validate parity conversion via global minimum - Optimize space complexity to O(1) by replacing the odd-element vector with a simple counter variable. use return (oddcout==0||oddcout==nums1.size()||min%2==1); to make the code more concise

--- LeetCode/Weekly-Contest-494-Q2.cpp
+++ LeetCode/Weekly-Contest-494-Q2.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        vector<int>v;
+        int oddcout=0;
         int min=nums1[0];
         for(int i:nums1)
         {
-            if(i%2==1) v.push_back(i);
+            if(i%2==1) oddcout++;
             if(i<min) min=i;
         }
-        int n=v.size();
-        if(n==0||n==nums1.size()||min%2==1) return true;
-        return false;
-        
+        return (oddcout==0||oddcout==nums1.size()||min%2==1);      
     }
 };©leetcode
