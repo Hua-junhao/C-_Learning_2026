b1d299480db0096a44612f4dc9c68e6e1dcd73ff
Hua-junhao <1249639696@qq.com>
2026-04-13 10:02:23 +0800

    perf(backtrack): [LC37] optimize Sudoku validation to O(1) via state pre-computation
    - Introduce 3D stack-allocated boolean arrays to track row, column, and sub-box occupancy.
    - Implement an initial board scan to pre-compute and populate existing constraints before recursive exploration.
    - Eliminate O(N) linear scans during branch validation, drastically reducing total backtracking execution time.

--- LeetCode/37.sudoku-solver.cpp
+++ LeetCode/37.sudoku-solver.cpp
@@ -7,20 +7,22 @@
 // @lc code=start
 class Solution {
 public:
-        bool backtracking(vector<vector<char>>&board)
+        bool backtracking(vector<vector<char>>&board,bool row[9][10],bool col[9][10],bool block[3][3][10])
         {
-            for(int i=0;i<board.size();++i)
+            for(int i=0;i<9;++i)
             {
-                for(int j=0;j<board[0].size();++j)
+                for(int j=0;j<9;++j)
                 {
                     if(board[i][j]!='.') continue;
                     for(char k='1';k<='9';k++)
                     {
-                        if(isValid(i,j,k,board)) 
+                        if(!row[i][k-'0'] && !col[j][k-'0'] && !block[i/3][j/3][k-'0']) 
                         {
                             board[i][j]=k;
-                            if(backtracking(board)) return true;
+                            row[i][k-'0']=col[j][k-'0']=block[i/3][j/3][k-'0']=true;
+                            if(backtracking(board,row,col,block)) return true;
                             board[i][j]='.';
+                            row[i][k-'0']=col[j][k-'0']=block[i/3][j/3][k-'0']=false; 
                         }
                     }
                     return false;
@@ -28,28 +30,21 @@ public:
             }
             return true;
         }
-        bool isValid(int r,int c,char k,const vector<vector<char>>&board)
-        {
-            for(int i=0;i<board[0].size();++i)
-            {
-                if(board[r][i]==k) return false;
-            }
-            for(int i=0;i<board.size();++i)
-            {
-                if(board[i][c]==k) return false;
-            }
-            r=(r/3)*3,c=(c/3)*3;
-            for(int i=r;i<r+3;++i)
-            {
-                for(int j=c;j<c+3;++j)
-                {
-                    if(board[i][j]==k) return false;
+    void solveSudoku(vector<vector<char>>& board) {
+        bool row[9][10]={false};
+        bool col[9][10]={false};
+        bool block[3][3][10]={false};
+        for(int i = 0; i < 9; ++i) {
+            for(int j = 0; j < 9; ++j) {
+                if(board[i][j] != '.') {
+                    int num = board[i][j] - '0';
+                    row[i][num] = true;
+                    col[j][num] = true;
+                    block[i/3][j/3][num] = true;
                 }
             }
-            return true;
         }
-    void solveSudoku(vector<vector<char>>& board) {
-        backtracking(board);
+        backtracking(board,row,col,block);
     }
 };
 // @lc code=end
