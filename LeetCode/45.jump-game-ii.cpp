bde48ae645e62bb9ec5e04ac7f780ffef9615c54
Hua-junhao <1249639696@qq.com>
2026-04-26 11:14:10 +0800

    feat(greedy):[LC45] implement optimal O(N) minimum jump sequence
    - Track `curcover` to represent the boundary of the current jump step.
    - Dynamically expand `nextcover` to find the furthest reachable index for the next jump.
    - Increment the jump `step` exclusively when the iterator reaches the current boundary.
    - Achieve O(N) time and O(1) space complexity.

---
+++ LeetCode/45.jump-game-ii.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=45 lang=cpp
+ *
+ * [45] Jump Game II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int step=0,n=nums.size(),curcover=0,nextcover=0;
+        if(n<=1) return step;
+        for(int i=0;i<n-1;++i)
+        {
+            nextcover=max(nextcover,nums[i]+i);
+            if(i==curcover)
+            {
+                step++;
+                curcover=nextcover;
+            }
+        }
+        return step;
+        
+    }
+};
+// @lc code=end
+
