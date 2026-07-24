a3f9b228d5be74f1c1b970d0cd3507be61e63252
Hua-junhao <1249639696@qq.com>
2026-07-24 19:22:04 +0800

    feat(dp): [LC474] implement 2D-cost 0/1 knapsack for ones and zeroes
    - Establish a 2D state matrix `dp[m+1][n+1]` where `dp[i][j]` represents the maximum subset size under dual constraints of `m` zeros and `n` ones.
    - Loop through each string in the input vector and manually count the frequencies of '0' and '1' characters.
    - Optimize the state space from 3D to 2D using the rolling array technique, iterating backwards to prevent duplicate item usage.

---
+++ LeetCode/474.ones-and-zeroes.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=474 lang=cpp
+ *
+ * [474] Ones and Zeroes
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(string str:strs)
+        {
+            int zeronum=0,onenum=0;
+            for(char c:str)
+            {
+                if(c=='0') zeronum++;
+                else onenum++;
+            }
+            for(int i=m;i>=zeronum;i--)
+            {
+                for(int j=n;j>=onenum;j--)
+                {
+                    dp[i][j]=max(dp[i][j],dp[i-zeronum][j-onenum]+1);
+                }
+            }
+        }
+        return dp[m][n];
+        
+    }
+};
+// @lc code=end
+
