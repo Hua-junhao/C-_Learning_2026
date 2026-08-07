df4376055474bf2e76e2580a5ee231a3b7b6d0ee
Hua-junhao <1249639696@qq.com>
2026-08-07 15:07:12 +0800

    perf(dp): [Kama57] eliminate inner loop branch and fix step boundary
    - Correct inner loop step initialization from `j = 0` to `j = 1` to logically represent valid $1...m$ step bounds.
    - Integrate `j <= i` directly into the inner loop condition, completely eliminating the conditional `if (i >= j)` branch inside the hot path.

--- DSA_Practice/kama-57-climbing-stairs.cpp
+++ DSA_Practice/kama-57-climbing-stairs.cpp
@@ -3,16 +3,13 @@ using namespace std;
 int main(){
     int n,m;
     cin>>n>>m;
-    vector<int>dp(n+1);
+    vector<int>dp(n+1,0);
     dp[0]=1;
     for(int i=1;i<=n;++i)
     {
-        for(int j=0;j<=m;++j)
+        for(int j=0;j<=m && j<=i;++j)
         {
-            if(i>=j)
-            {
-                dp[i]+=dp[i-j];
-            }
+            dp[i]+=dp[i-j];
         }
     }
     cout<<dp[n]<<endl;
